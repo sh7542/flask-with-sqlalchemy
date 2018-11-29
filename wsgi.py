6a05c980c8733986b30f51550a71eee411690f2a
sylvain hervy <sylvain.hervy@gmail.com>
2018-11-29 10:58:28 +0100

    First Deployment to Heroku

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+# wsgi.py
+from flask import Flask
+from config import Config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow # Order is important here!
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+from models import Product
+from schemas import products_schema
+
+@app.route('/products')
+def products():
+    products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
+    return products_schema.jsonify(products)
